6560fa917b1b0f1a8564fe071cf032acc14b2728
Arthur <Arthur@Arthur-Laptop.niacc.edu>
2020-10-23 09:54:58 -0500

    Assignment Loop 2 finished.

--- public_html/GradingMaster.js
+++ public_html/GradingMaster.js
@@ -10,7 +10,32 @@
  *      
  * BONUS: Get scores from the user instead of initializing them in the program
  */
+let gradeScores = [55, 65, 75, 85, 95]; 
 
-document.write("<p>Grade calculation program</p>");
+let addedScores = 0;
+
+for (index = 0; index < gradeScores.length; index++) {
+    addedScores = gradeScores[index] + addedScores;
+}
+
+let averageScore = addedScores/gradeScores.length;
+
+let aGrade;
+
+if (averageScore >= 90) {
+    aGrade = "A";
+} else if (averageScore >= 80) {
+    aGrade = "B";
+} else if (averageScore >= 70) {
+    aGrade = "C";
+} else if (averageScore >= 60) {
+    aGrade = "D";
+} else {
+    aGrade = "F";
+}
+
+document.write(aGrade);
+
+
 
 
